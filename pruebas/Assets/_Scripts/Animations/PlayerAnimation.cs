a6f4b8097b50678c02643a17e2bfaab3913ccbbd
Nico <nicolasdrr25@gmail.com>
2026-04-20 22:46:24 -0500

    fix: meow sound only in cat form, plant enemy tag, crystal power amount

--- pruebas/Assets/_Scripts/Animations/PlayerAnimation.cs
+++ pruebas/Assets/_Scripts/Animations/PlayerAnimation.cs
@@ -8,12 +8,14 @@ public class PlayerAnimation : MonoBehaviour
     private Animator _animator;
     private NewInput _newInput;
     private CatMeowSound _catMeowSound;
+    private CatTransform _catTransform;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
         _newInput = GetComponent<NewInput>();
         _catMeowSound = GetComponent<CatMeowSound>();
+        _catTransform = GetComponent<CatTransform>();
     }
 
     // Update is called once per frame
@@ -25,14 +27,17 @@ public class PlayerAnimation : MonoBehaviour
 
     public void MovementAnim()
     {
+        bool isCat = _catTransform != null && _catTransform.IsCat;
+
         if(_newInput.inputX > 0 || _newInput.inputX < 0)
         {
             _animator.SetBool("Bool", true);
-            _catMeowSound?.OnCatMovement();
-        }else if(_newInput.inputX == 0)
+            if (isCat) _catMeowSound?.OnCatMovement();
+        }
+        else if(_newInput.inputX == 0)
         {
             _animator.SetBool("Bool", false);
-            _catMeowSound?.OnCatIdle();
+            if (isCat) _catMeowSound?.OnCatIdle();
         }
     }
 }
